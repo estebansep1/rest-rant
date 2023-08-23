a359e86b66cbc2f7f6a87aa1637ce5e62304df5e
Esteban Sepulveda <tebansd@icloud.com>
2023-08-22 22:38:47 -0700

    added bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div className="errorImg">
+                    <img src="/images/restaurant.jpg" alt="Picture of restaurant"/> 
+                </div>
+                <div>
+                Photo by <a href="https://unsplash.com/@chrisliverani">Brenda Godinez</a> on <a href="https://unsplash.com/">Unsplash</a>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -1,13 +1,19 @@
 const React = require('react')
-const Def = require('./places/default') 
+const Def = require('./places/default')
 
 function home () {
     return (
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                <img src="/images/chia-yogurt.jpg" alt="Chia Fruit Shake" />
+                <div>
+                    Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/">Unsplash</a>
+                </div>
+                </div>
                 <a href="/places">
-                <button className="btn-primary">Places Page</button>
+                    <button className="btn-primary">Places Page</button>
                 </a>
             </main>
         </Def>
