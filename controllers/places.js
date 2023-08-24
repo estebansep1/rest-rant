2b2ead042a5215561dc9477bb7bd2e4e188c3056
Esteban Sepulveda <tebansd@icloud.com>
2023-08-24 14:51:02 -0700

    added footer

--- controllers/places.js
+++ controllers/places.js
@@ -1,44 +1,27 @@
-const router = require('express').Router()
-const places = require('../models/places.js')
+const router = require('express').Router();
+const places = require('../models/places.js');
 
 router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+  res.render('places/new');
+});
 
-router.get('/', (req, res) =>{
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/brunch.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee.jpg'
-      }]
-      
-    res.render('places/index', { places })
-})
-  
+router.get('/', (req, res) => {
+  res.render('places/index', { places: places });
+});
+
 router.post('/', (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = 'http://placekitten.com/400/400';
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = 'Anytown';
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state = 'USA';
   }
-  places.push(req.body)
-  res.redirect('/places')
-})
+  places.push(req.body);
+  res.redirect('/places');
+});
 
-
-
-module.exports = router 
+module.exports = router;
--- views/default.jsx
+++ views/default.jsx
@@ -6,11 +6,18 @@ function Def (html) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
-                <link rel="stylesheet" href="/style.css"/>
+                <link rel="stylesheet" href="../style.css"/>
             </head>
             <body>
                 {html.children}
             </body>
+            <footer className="stickyFooter">
+                <p>Social Links</p>
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/esteban-sepulveda-a06789161/" >LinkedIn</a></li>
+                    <li><a href="https://github.com/estebansep1">Github</a></li>
+                </ul>
+            </footer>
         </html>
     )
 }
