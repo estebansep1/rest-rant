b35f92dea69b21a84d13c93516229ebf0b3cddf6
Esteban Sepulveda <tebansd@icloud.com>
2023-08-24 21:25:48 -0700

    delete functionality implemented:

--- controllers/places.js
+++ controllers/places.js
@@ -10,7 +10,6 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  console.log('Reached the show route'); // A
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
@@ -19,7 +18,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] });
+    res.render('places/show', { place: places[id], id })
   }
 })
 
@@ -37,5 +36,21 @@ router.post('/', (req, res) => {
   places.push(req.body);
   res.redirect('/places');
 });
+
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
 
 module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,37 @@
-const React = require('react')
-const Def = require('../default')
+const React = require('react');
+const Def = require('../default');
 
-function show () {
+function show(data) {
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
+            <main>
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src={data.place.pic} alt={data.place.name} className="img-fluid" />
+                        <h3>Located in {data.place.city}, {data.place.state}</h3>
+                    </div>
+                    <div className="col-md-6">
+                        <h1>{data.place.name}</h1>
+                        <h2>Rating</h2>
+                        <p>Not Rated</p>
+                        <h2>Description</h2>
+                        <p>{data.place.description}</p>
+                        <h4>Serving {data.place.cuisines}</h4>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>     
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                        <button type="submit" className="btn btn-danger">Delete</button>
+                        </form> 
+
+                    </div>
+                </div>
+                <hr />
+                <h2>Comments</h2>
+                <p>No comments yet!</p>
+            </main>
         </Def>
-    )
+    );
 }
 
-module.exports = show
+module.exports = show;
+
+
