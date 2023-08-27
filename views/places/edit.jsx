ee8e55026324420dd9a9be3ceddc6ba70eb411e6
Esteban Sepulveda <tebansd@icloud.com>
2023-08-27 11:24:37 -0700

    fixed form layout

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,45 +1,35 @@
-const React = require('react')
-const Def = require('../default.jsx')
+const React = require('react');
+const Def = require('../default.jsx');
 
 function edit_form(data) {
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <div className="form-group">
-                                <label htmlFor="name">Place Name</label>
-                                <input className="form-control" id="name" name="name" value={data.place.name} required />
-                            </div>
-                        </div>
-                        <div className="col-sm-6">
-                            <div className="form-group">
-                                <label htmlFor="pic">Place Picture</label>
-                                <input className="form-control" id="pic" name="pic" value={data.place.pic} />
-                            </div>
-                        </div>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`} className="row g-3">
+                    <div className="col-md-6">
+                        <label htmlFor="name" className="form-label">Place Name</label>
+                        <input type="text" className="form-control" id="name" name="name" value={data.place.name} required />
                     </div>
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <div className="form-group">
-                                <label htmlFor="city">City</label>
-                                <input className="form-control" id="city" name="city" value={data.place.city} />
-                            </div>
-                        </div>
-                        <div className="col-sm-6">
-                            <div className="form-group">
-                                <label htmlFor="state">State</label>
-                                <input className="form-control" id="state" name="state" value={data.place.state} />
-                            </div>
-                        </div>
+                    <div className="col-md-6">
+                        <label htmlFor="pic" className="form-label">Place Picture</label>
+                        <input type="text" className="form-control" id="pic" name="pic" value={data.place.pic} />
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                    <div className="col-md-6">
+                        <label htmlFor="city" className="form-label">City</label>
+                        <input type="text" className="form-control" id="city" name="city" value={data.place.city} />
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Update Place" />
+                    <div className="col-md-6">
+                        <label htmlFor="state" className="form-label">State</label>
+                        <input type="text" className="form-control" id="state" name="state" value={data.place.state} />
+                    </div>
+                    <div className="col-md-6">
+                        <label htmlFor="cuisines" className="form-label">Cuisines</label>
+                        <input type="text" className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                    </div>
+                    <div className="col-12">
+                        <button type="submit" className="btn btn-primary">Update Place</button>
+                    </div>
                 </form>
             </main>
         </Def>
