92644792d683f4f051746b69f46a67c4cb340c0e
Esteban Sepulveda <tebansd@icloud.com>
2023-09-14 11:38:15 -0700

    added mongo connection string

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
-})
+}) .then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) })
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comment')
